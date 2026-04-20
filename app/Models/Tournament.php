a35ead1aa055d9a21b416c253df24d4a1e33cca9
RizkyRR27 <rizkyroza2005@gmail.com>
2026-04-20 10:47:36 +0700

    benarkan tanggal pt2

--- app/Models/Tournament.php
+++ app/Models/Tournament.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Tournament extends Model
 {
@@ -15,4 +16,36 @@ class Tournament extends Model
         'start_date' => 'date',
         'end_date' => 'date',
     ];
+
+    public static function findBlockingEvent($date)
+    {
+        $userDate = Carbon::parse($date);
+
+        $oneTimeEvent = self::where('is_recurring', false)
+            ->whereDate('start_date', '<=', $userDate)
+            ->whereDate('end_date', '>=', $userDate)
+            ->first();
+
+        if ($oneTimeEvent) {
+            return $oneTimeEvent;
+        }
+
+        $recurringEvents = self::where('is_recurring', true)->get();
+
+        foreach ($recurringEvents as $event) {
+            $start = Carbon::parse($event->start_date)
+                ->setYear($userDate->year)
+                ->startOfDay();
+
+            $end = Carbon::parse($event->end_date)
+                ->setYear($userDate->year)
+                ->endOfDay();
+
+            if ($userDate->between($start, $end)) {
+                return $event;
+            }
+        }
+
+        return null;
+    }
 }
